02a9ab6b4fbb1d04fe50d6921d3870c49626ab30
ozvar <ov525@york.ac.uk>
2023-10-30 12:57:16 +0000

    removed redundant params, modified to allow specification for multiple experiments

--- monitor/parameters.py
+++ monitor/parameters.py
@@ -1,13 +1,32 @@
-# monitor parameters
-TRANSF = [0, 0.2, 0.5, 0.8, 1]
-EPS_SCALARS = [0.8, 1.0, 0.6]
-ACC_BOUNDS = [0.70, 0.40]
-K_FOLDS = 3
-
-# in- and output directories
-DATA_DIR = 'transformations'
-MODEL_DIR = 'models'
-
-# target dataset and model for the monitor
-DATASET = 'gtsrb'
-MODEL = 'model3b.h5'
+experiments = [
+    {
+        "TRANSF_FACTORS": {
+            "haze": 0.8,
+            "blur": 0.6
+        },
+        "EPSILONS": [0, 0.2, 0.5, 0.8, 1],
+        "ACC_BOUNDS": [0.70, 0.40],
+        "K_FOLDS": 5,
+        "RUNS": 5,
+        "DATA_DIR": "transformations",
+        "MODEL_DIR": "models",
+        "FIG_DIR": "results/figures",
+        "DATASET": "gtsrb",
+        "MODEL": "model3b.h5"
+    },
+    {
+        "TRANSF_FACTORS": {
+            "blur": 0.6,
+            "haze": 0.8
+        },
+        "EPSILONS": [0, 0.2, 0.5, 0.8, 1],
+        "ACC_BOUNDS": [0.70, 0.40],
+        "K_FOLDS": 5,
+        "RUNS": 5,
+        "DATA_DIR": "transformations",
+        "MODEL_DIR": "models",
+        "FIG_DIR": "results/figures",
+        "DATASET": "gtsrb",
+        "MODEL": "model3b.h5"
+    }
+]
