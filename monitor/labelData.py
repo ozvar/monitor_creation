a5a87e594ef78f0e9b7fc7af70a78655dc281d5f
ozvar <ov525@york.ac.uk>
2023-11-02 08:29:18 +0000

    changed pathing for files and cleaned up

--- monitor/labelData.py
+++ monitor/labelData.py
@@ -3,6 +3,7 @@ import numpy as np
 import os, glob
 import pandas as pd
 from pathlib import Path
+from tqdm import tqdm
 from datasets import load_gtsrb, load_cifar 
 from parameters import *
 
@@ -16,7 +17,11 @@ def _sortfunc(j: str) -> int:
     return el
 
 
-def computePerformance(model: tf.keras.models, data_dir: str, labels: pd.DataFrame) -> np.array:
+def computePerformance(
+        model: tf.keras.models,
+        data_dir: str,
+        labels: pd.DataFrame
+        ) -> np.array:
     list_files = [f for f in glob.glob(os.path.join(data_dir, '*.npy')) 
                   if os.path.basename(f).startswith('data')]
     accuracy = np.zeros(len(list_files))
@@ -30,36 +35,46 @@ def computePerformance(model: tf.keras.models, data_dir: str, labels: pd.DataFra
     return accuracy
 
 
-def labelDataset (accuracy : np.array, label : np.array) -> list:
-
+def label_dataset(
+        accuracy: np.array,
+        labels: np.array
+        ) -> list:
     labData=[]
     for i in range(len(accuracy)):
-        for j in range(len(label)):
-            if (j == 0 and accuracy[i] >= label[j]):
+        for j in range(len(labels)):
+            if (j == 0 and accuracy[i] >= labels[j]):
                 labData.append((accuracy[i], j))
                 break
-            elif (j != 0 and label[j - 1] > accuracy[i] >= label[j]):
+            elif (j != 0 and labels[j - 1] > accuracy[i] >= labels[j]):
                 labData.append((accuracy[i], j))
-            elif (j == len(label)-1 and accuracy[i]< label[j]):
+            elif (j == len(labels)-1 and accuracy[i]< labels[j]):
                 labData.append((accuracy[i], j+1))
     return labData
 
 
-def compute_and_save_accuracies(model_dir: Path, dataset: str, data_dir: Path, acc_bounds: list):
-    model = tf.keras.models.load_model(model_dir / dataset / MODEL)
-
-    # Load data based on the dataset name
+def compute_and_save_accuracies(
+        model_dir: Path,
+        dataset: str,
+        model_name: str,
+        data_dir: Path,
+        acc_bounds: list):
+    # load model
+    model_dir, data_dir = Path(model_dir), Path(data_dir)
+    model = tf.keras.models.load_model(model_dir / dataset / model_name)
+    # load data based on the dataset name
     if dataset == 'gtsrb':
         X_train, y_train, X_test, y_test, labels = load_gtsrb()
-        print(y_test)
     else:
         raise ValueError(f"Unsupported dataset: {dataset}")
-
     # compute performance
-    acc = computePerformance(model, data_dir, y_test)
+    acc = computePerformance(
+            model=model,
+            data_dir=data_dir,
+            labels=y_test)
     np.save(data_dir / 'accuracy', acc)
-
     # label datasets 
     labels = np.array(acc_bounds)
-    labData = labelDataset(acc, labels)
+    labData = label_dataset(
+            accuracy=acc,
+            labels=labels)
     np.save(data_dir / 'labDatasets', labData)
