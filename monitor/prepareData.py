40011ec017e3c95efff28fa1ee1b4b5aec53ee83
ozvar <ov525@york.ac.uk>
2024-01-03 19:25:57 +0000

    revised data sampling to train on all samples rather than randomly selected datasets, also added image generation for visual inspection of samples

--- monitor/prepareData.py
+++ monitor/prepareData.py
@@ -1,35 +1,36 @@
 import numpy as np
 import gc
 import typing
-import random
+import os
 import pickle
 from tqdm import tqdm
 from pathlib import Path
 from tensorflow.keras.utils import to_categorical
 from utils import setup_logger
+from utils import generate_images_from_dataset
 
 
 def count_samples_classes(
-        labData: list,
+        lab_data: list,
         num_classes: int
         ) -> np.array:
     count_classes = np.zeros(num_classes)
-    for el in labData:
+    for el in lab_data:
         i = el[1]
         count_classes[int(i)] += 1
     return count_classes
 
 
 def find_indexes(
-        labData: list,
+        lab_data: list,
         cl: int,
         num_classes: int
         )-> np.array:
-    num_samples = count_samples_classes(labData, num_classes)
+    num_samples = count_samples_classes(lab_data, num_classes)
     ind = np.zeros(int(num_samples[cl]))
     j = 0
-    for i in range(len(labData)):
-        if labData[i][1] == cl:
+    for i in range(len(lab_data)):
+        if lab_data[i][1] == cl:
             ind[j] = i
             j += 1
     return ind
@@ -92,11 +93,11 @@ def shuffle_arrays(
 def process_class(
         class_idx: int,
         transf_dir: Path,
-        labData: np.array,
+        lab_data: np.array,
         ntrainind: int,
         ntestind: int,
         num_classes: int):
-    indexes_train, indexes_test = find_samples(find_indexes(labData, class_idx, num_classes), ntrainind, ntestind)
+    indexes_train, indexes_test = find_samples(find_indexes(lab_data, class_idx, num_classes), ntrainind, ntestind)
     images_train = conc_samples(transf_dir, indexes_train)
     labels_train = np.full(len(images_train), class_idx)
     images_test = conc_samples(transf_dir, indexes_test)
@@ -105,34 +106,67 @@ def process_class(
     return (images_train, labels_train, images_test, labels_test, indexes_train, indexes_test)
 
 
+def prepare_class(
+        class_idx: int,
+        train_prop: float,
+        transf_dir: Path,
+        lab_data: np.array):
+
+    samples_list = []
+    index_list = []
+
+    for i, (perf, label) in enumerate(lab_data):
+        if label == class_idx:
+            file_path = transf_dir / f"data{int(i)}.npy"
+            loaded_data = np.load(file_path)
+            samples_list.append(loaded_data)
+            index_list.append(np.full(len(loaded_data), i))
+
+    # Concatenate all samples and indices
+    samples = np.concatenate(samples_list, axis=0)
+    train_ind = np.concatenate(index_list, axis=0)
+
+    # Shuffle and split the dataset
+    indices = np.arange(len(samples))
+    np.random.shuffle(indices)
+    samples, train_ind = samples[indices], train_ind[indices]
+
+    split_idx = int(train_prop * len(samples))
+    train_samples, test_samples = samples[:split_idx], samples[split_idx:]
+    train_ind, test_ind = train_ind[:split_idx], train_ind[split_idx:]
+    labels_train = np.full(len(train_samples), class_idx)
+    labels_test = np.full(len(test_samples), class_idx)
+
+    return train_samples, labels_train, test_samples, labels_test, train_ind, test_ind
+
+
 def prepare_and_save_data(
         out_dir: Path,
+        image_dir: Path,
         transf_dir: Path,
-        ntrainind: int,
-        ntestind: int,
+        train_prop: float,
         acc_bounds: list,
+        imageind: list,
         run_id: int,
         log_label: str='data'):
     # setup logger
     logger = setup_logger(out_dir, run_id, log_label)
     # load variables
-    labData = np.load(transf_dir/ 'labDatasets.npy')
+    lab_data = np.load(transf_dir/ 'labDatasets.npy')
     acc = np.load(transf_dir / 'accuracy.npy')
     num_classes = len(acc_bounds) + 1
     params = {
         "class_idx": None,
         "transf_dir": transf_dir,
-        "labData": labData,
-        "ntrainind": ntrainind,
-        "ntestind": ntestind,
-        "num_classes": num_classes
+        "lab_data": lab_data,
+        "train_prop": train_prop
     }
     # randomly sample datasets from each class    
     pbar = tqdm(range(num_classes), position=0)
     results = []
     for i in pbar:
         pbar.set_description(f"Processing class {i+1}/{num_classes}")
-        results.append(process_class(**{**params, "class_idx": i}))
+        results.append(prepare_class(**{**params, "class_idx": i}))
     all_trainX, all_trainY, all_testX, all_testY, all_trainind, all_testind = zip(*results)
 
     trainX, trainY = np.concatenate(all_trainX), to_categorical(np.concatenate(all_trainY))
@@ -145,23 +179,32 @@ def prepare_and_save_data(
     with open(out_dir / 'indexes.pickle', 'wb') as f:
         pickle.dump([trainind, testind], f)
     # describe class boundaries and number of samples per class
-    counts = count_samples_classes(labData, num_classes)
+    counts = count_samples_classes(lab_data, num_classes)
     logger.info(f"Class boundaries: {acc_bounds}")
     logger.info(f"Number of samples per class: {counts}")
-    logger.info("Indexes of training datasets:")
-    for i in range(num_classes):
-        logger.info(f"Class {i+1}:")
-        train_acc = [acc[int(j)] for j in all_trainind[i]]
-        logger.info(f"Accuracy: {train_acc}")
-        logger.info(f"Indexes: {all_trainind[i]}")
-    logger.info("Indexes of test datasets:")
-    for i in range(num_classes):
-        logger.info(f"Class {i+1}:")
-        test_acc = [acc[int(j)] for j in all_testind[i]]
-        logger.info(f"Accuracy: {test_acc}")
-        logger.info(f"Indexes: {all_testind[i]}")
-
-
+    # generate images from training and test datasets
+    for i, inds in enumerate(all_trainind):
+        run_image_dir = image_dir / f'run_{run_id+1}_train'
+        os.makedirs(run_image_dir, exist_ok=True)
+        generate_images_from_dataset(
+                transf_dir=transf_dir,
+                data_set=f'data{int(inds[0])}.npy', 
+                acc_class=f'class_{i+1}',
+                indexes=imageind,
+                image_size=(128, 128),
+                out_dir=run_image_dir
+                )
+    for i, inds in enumerate(all_testind):
+        run_image_dir = image_dir / f'run_{run_id+1}_test'
+        os.makedirs(run_image_dir, exist_ok=True)
+        generate_images_from_dataset(
+                transf_dir=transf_dir,
+                data_set=f'data{int(inds[0])}.npy', 
+                acc_class=f'class_{i+1}',
+                indexes=imageind,
+                image_size=(128, 128),
+                out_dir=run_image_dir
+                )
     # close the logger
     for handler in logger.handlers:
         handler.close()
