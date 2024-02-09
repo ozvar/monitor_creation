d7e35f6282a22005c6db44d35130ef33a53b541e
ozvar <ov525@york.ac.uk>
2024-02-09 14:03:16 +0000

    reduced num hidden layers, added spatial dropout layers, added function to initialize bias properly, reset random seed before training and testing, plot confusion matrix during testing

--- monitor/createMonitor.py
+++ monitor/createMonitor.py
@@ -7,6 +7,8 @@ from pathlib import Path
 from sklearn.model_selection import KFold
 from sklearn.metrics import classification_report
 from utils import setup_logger
+from utils import reset_random_seeds
+from visualization import plot_metrics, plot_confusion_matrix
 
 
 def loadData(data_dir: Path) -> typing.Tuple[np.array, np.array, np.array, np.array]:
@@ -15,35 +17,40 @@ def loadData(data_dir: Path) -> typing.Tuple[np.array, np.array, np.array, np.ar
     return trainX, trainY, testX, testY
 
 
-def create_model():
+def create_model(output_bias):
     return tf.keras.Sequential(
         [
             tf.keras.Input(shape=(32, 32, 3)),
             tf.keras.layers.Conv2D(16, 3, activation='relu'),
-            tf.keras.layers.SpatialDroput2D(0.1),
+            tf.keras.layers.SpatialDropout2D(0.3),
             tf.keras.layers.Conv2D(32, 3, activation='relu'),
-            tf.keras.layers.SpatialDroput2D(0.1),
-            tf.keras.layers.Conv2D(64, 3, activation='relu'),
-            tf.keras.layers.SpatialDroput2D(0.1),
-            tf.keras.layers.Conv2D(128, 3, activation='relu'),
-            tf.keras.layers.SpatialDroput2D(0.1),
-            tf.keras.layers.Conv2D(256, 3, activation='relu'),
-            tf.keras.layers.SpatialDroput2D(0.1),
+            tf.keras.layers.SpatialDropout2D(0.3),
             tf.keras.layers.GlobalAveragePooling2D(),
-            tf.keras.layers.Dense(3, activation='softmax')
+            tf.keras.layers.Dense(3,
+                                  activation='softmax',
+                                  bias_initializer=output_bias)
         ]
     )
 
 
-def init_bias(model: tf.keras.models) -> None:
-    pass
+def init_bias(labels: np.array) -> np.array:
+    class_frequencies = np.sum(labels, axis=0)
+    total_samples = sum(class_frequencies)
+    probabilities = [freq / total_samples for freq in class_frequencies]
+    initial_biases = [np.log(prob / (1 - prob)) for prob in probabilities]
+    initial_biases = tf.keras.initializers.Constant(initial_biases)
+
+    return initial_biases
         
 
 def train_monitor(
         model_dir: str,
         data_dir: Path,
+        fig_dir: Path,
         k_folds: int,
         batch_size: int,
+        epochs: int,
+        seed: int,
         run_id: int,
         log_label: str='train'):
     # initialize logging and paths
@@ -57,31 +64,34 @@ def train_monitor(
         # clear the session
         tf.keras.backend.clear_session()
         # implement early stopping
-        # early_stopping = tf.keras.callbacks.EarlyStopping(
-        #     monitor='val_loss',
-        #     patience=2,
-        #     restore_best_weights=True
-        # )
-
+        early_stopping = tf.keras.callbacks.EarlyStopping(
+             monitor='val_loss',
+             verbose=0,
+             patience=2,
+             restore_best_weights=True
+        )
         # calling the model and compile it
-        seq_model = create_model()
+        reset_random_seeds(seed)
+        output_bias = init_bias(trainY[train_idx])
+        seq_model = create_model(output_bias)
         seq_model.compile(
             loss  = tf.keras.losses.CategoricalCrossentropy(),
-            metrics  = tf.keras.metrics.CategoricalAccuracy(),
+            metrics = tf.keras.metrics.CategoricalAccuracy(),
             optimizer = tf.keras.optimizers.Adam())
 
         log_message = (f'K-Fold: {k+1}\n'
                        f'Train Set: {trainX[train_idx].shape}, {trainY[train_idx].shape}\n'
                        f'Test Set: {trainX[test_idx].shape}, {trainY[test_idx].shape}')
         logger.info(log_message)
-
         # run the model
-        seq_model.fit(trainX[train_idx], trainY[train_idx],
+        history = seq_model.fit(trainX[train_idx], trainY[train_idx],
                 batch_size=batch_size,
-                epochs=2,
+                epochs=epochs,
                 validation_data=(trainX[test_idx], trainY[test_idx]))
         seq_model.save_weights(model_dir / f'wg_{k+1}_2.h5')
-
+        # Plot and save metrics
+        plot_metrics(history, run_id, k+1, fig_dir, metric='loss')
+        plot_metrics(history, run_id, k+1, fig_dir, metric='categorical_accuracy')
         # log model training accuracy
         train_acc = seq_model.evaluate(trainX[train_idx], trainY[train_idx])
         logger.info(f'Train Accuracy: {train_acc}')
@@ -102,10 +112,12 @@ def train_monitor(
 
 
 def test_monitor(
-        model_dir: str,
+        model_dir: Path,
         data_dir: Path,
         k_folds: int,
+        seed: int,
         run_id: int,
+        fig_dir: Path,
         log_label: str='test'):
     # initialize logging and paths
     logger = setup_logger(data_dir, run_id, log_label)
@@ -114,7 +126,8 @@ def test_monitor(
     [_, _, testX, testY] = loadData(data_dir)
     logger.info('Evaluating model...')
     for k in range(k_folds):
-        model = create_model()
+        reset_random_seeds(seed)
+        model = create_model(output_bias='zeros')
         model.compile(
             loss=tf.keras.losses.CategoricalCrossentropy(),
             metrics=tf.keras.metrics.CategoricalAccuracy(),
@@ -126,9 +139,11 @@ def test_monitor(
         # log model test accuracy
         test_acc = model.evaluate(testX, testY)
         logger.info(f'Test Accuracy for k={k+1}: {test_acc}')
-        # log classification report
         ohe_testY = np.argmax(testY, axis=1)
         ohe_predY = np.argmax(model.predict(testX), axis=1)
+        # plot confusion matrix
+        plot_confusion_matrix(ohe_testY, ohe_predY, run_id, k+1, fig_dir)
+        # log classification report
         logger.info(f'Classification Report for k={k+1}:')
         logger.info(classification_report(ohe_testY, ohe_predY))
     
